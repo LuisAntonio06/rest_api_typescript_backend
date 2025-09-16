4c3b6fd79960bdf126a364061dc3175825b2f020
Wuiss <luiszam2000@gmail.com>
2025-09-16 17:23:36 -0600

    Fixed db

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,10 +1,16 @@
 import { Sequelize } from "sequelize-typescript";
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
 dotenv.config();
 
-const db = new Sequelize(process.env.DATABASE_URL! , {
-    models: [__dirname + '/../models/**/*'],
-    logging: false
-})
+const db = new Sequelize(process.env.DATABASE_URL!, {
+    models: [__dirname + '/../models/**/*.ts', __dirname + '/../models/**/*.js'],
+    logging: false,
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    }
+});
 
 export default db;
