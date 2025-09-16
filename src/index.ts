7754f5c570c935f38c9780e5897fefa886824fc8
Wuiss <luiszam2000@gmail.com>
2025-09-16 16:49:16 -0600

    Fixed Package.json by deploy

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import colors from "colors";
 
 /* Definir puerto de la app */
 const port = process.env.PORT || 4000
-server.listen(4000 , () => {
+server.listen(port , () => {
     console.log(colors.blue.bold(`REST API EN EL PUERTO: ${port}`));
 })
 
